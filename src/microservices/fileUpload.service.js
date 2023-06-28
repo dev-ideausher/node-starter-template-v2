a8817403cd235aeece651a6203e3c9c271ca4dfa
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2023-06-28 11:42:39 +0530

    Update fileUpload.service.js
    Resolves #2

--- src/microservices/fileUpload.service.js
+++ src/microservices/fileUpload.service.js
@@ -1,22 +1,32 @@
 const uuid = require("uuid").v4;
-const multer = require('multer');
+const multer = require("multer");
 const storage = multer.memoryStorage();
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 
 const config = require("../config/config");
 const { incidentUpdateFileTypes } = require("../constants");
 const { accessKeyId, region, secretAccessKey, name } = config.aws.s3;
 
-const s3client = new S3Client({ region, credentials: { accessKeyId, secretAccessKey }});
+const s3client = new S3Client({
+  region,
+  credentials: { accessKeyId, secretAccessKey },
+});
 
 async function fileFilter(req, file, cb) {
   if (incidentUpdateFileTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file or data, only JPEG ,PNG and pdf is allowed!'), false);
+    cb(
+      new Error("Invalid file or data, only JPEG ,PNG and pdf is allowed!"),
+      false
+    );
   }
-};
+}
 
 const multerUpload = multer({
   storage,
@@ -24,30 +34,34 @@ const multerUpload = multer({
   limits: { fileSize: 1500000, files: 5 },
 });
 
-async function getObjectURL(Key) {
+async function getObjectURL(Key, signedUrl = false) {
   const command = new GetObjectCommand({ Key, Bucket: name });
   const url = await getSignedUrl(s3client, command);
-  return url;
+  return signedUrl ? url : url.split("?")[0];
 }
 
 async function s3Upload(files) {
   const params = files.map((file) => {
+    console.log(file);
     return {
       Bucket: name,
-      Key: `uploads/${uuid()}-${file.originalname}`,
+      Key: `${file?.folder || "uploads"}/${uuid()}-${file.originalname}`,
       Body: file.buffer,
     };
   });
 
   return await Promise.all(
-    params.map((param) => s3client.send(new PutObjectCommand(param))
-      .then(async () => getObjectURL(param.Key))
-      .catch(() => null)
-  ));
-};
+    params.map((param) =>
+      s3client
+        .send(new PutObjectCommand(param))
+        .then(async () => getObjectURL(param.Key))
+        .catch(() => null)
+    )
+  );
+}
 
 module.exports = {
   s3Upload,
   getObjectURL,
-  multerUpload
-}
+  multerUpload,
+};
