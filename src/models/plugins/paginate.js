81fbf478d305de8a2d29f79c894f2bf373808a3b
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2023-08-30 16:21:47 +0530

    fix: do not discard doc in populate mech if regex match is not present

--- src/models/plugins/paginate.js
+++ src/models/plugins/paginate.js
@@ -1,23 +1,19 @@
 const {default: mongoose} = require('mongoose');
 
 const paginate = schema => {
-  schema.statics.paginate = async function(
-    filters,
-    options,
-    geoFilters = null
-  ) {
+  schema.statics.paginate = async function(filters, options, geoFilters = null) {
     // defining pipeline
     const pipeline = [];
     if (geoFilters) {
-      const { longitude, latitude, radius = 1 } = geoFilters;
+      const {longitude, latitude, radius = 1} = geoFilters;
       pipeline.push({
         $geoNear: {
-            near: { type: "Point", coordinates: [ longitude, latitude ] },
-            distanceField: "dist.calculated",
-            maxDistance: 1609.34 * radius,
-            key: "location",
-        }
-      })
+          near: {type: 'Point', coordinates: [longitude, latitude]},
+          distanceField: 'dist.calculated',
+          maxDistance: 1609.34 * radius,
+          key: 'location',
+        },
+      });
     }
     // sepearte post populate filters from initial filters
     const {postPopulateFilters, ...initialFilters} = filters;
@@ -25,15 +21,9 @@ const paginate = schema => {
     pipeline.push({$match: initialFilters || {}});
 
     // defining the limit that user is requesting
-    const limit =
-      options.limit && parseInt(options.limit, 10) > 0
-        ? parseInt(options.limit, 10)
-        : 10;
+    const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
     // defining the page or offset user is requesting
-    const page =
-      options.page && parseInt(options.page, 10) > 0
-        ? parseInt(options.page, 10)
-        : 1;
+    const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
     // defining the skip count that user is requesting
     const skip = (page - 1) * limit;
 
@@ -41,34 +31,37 @@ const paginate = schema => {
     let docsPipeline = [...pipeline, ...(options.pipeline || [])];
 
     // adding location based search filters if given by user
-    
 
     // populating the fields if requested by user
     if (options.populate) {
       // pattern is `path::select1,select2:matchCondition2,select3`[]
       // e.g. "createdBy::name:tushar,email" => "createdBy::name:tushar,email"
       // converting to array if option is given in form of string
-      const populateOptions = Array.isArray(options.populate)
-        ? options.populate
-        : [options.populate];
+      const populateOptions = Array.isArray(options.populate) ? options.populate : [options.populate];
       // iterating over the populate options
       populateOptions.forEach(populateOption => {
         const matchObj = {};
         // seperating the path and fields that has to be selected from the the reffered collection
         const [path, select] = populateOption.split('::');
         // getting the name of collection where the field is referring to
-        const collectionName = mongoose.model(this.schema.obj[path].ref)
-          .collection.name;
+        const collectionName = mongoose.model(this.schema.obj[path].ref).collection.name;
+        let isMatchRequested = false;
         // iterating over the fields that has to be selected
-        const selectFields = select.split(',').map(ele => {
-          // seperating the requested select field from its match condition
-          // e.g. i want name field but its value should match with pattern "Manish"
-          // meaning extract documents from THIS collection who has "Manish" in the name field of the REFFERED collection
-          const [name, match] = ele.split(':');
-          // storing requested pattern match for a specific field
-          if (match) matchObj[name] = match;
-          return name;
-        });
+        const selectFields =
+          select === '*'
+            ? Object.keys(mongoose.model(this.schema.obj[path].ref).schema.obj)
+            : select.split(',').map(ele => {
+                // seperating the requested select field from its match condition
+                // e.g. i want name field but its value should match with pattern "Manish"
+                // meaning extract documents from THIS collection who has "Manish" in the name field of the REFFERED collection
+                const [name, match] = ele.split(':');
+                // storing requested pattern match for a specific field
+                if (match) {
+                  matchObj[name] = match;
+                  isMatchRequested = true;
+                }
+                return name;
+              });
 
         // defining lookup for the current path
         const lookup = {
@@ -129,15 +122,17 @@ const paginate = schema => {
             },
           },
         });
-        // applying filter to exclude the document
-        // if the above step hasn't populated the fields
-        docsPipeline.push({
-          $match: {
-            $expr: {
-              $ne: ['$$ROOT', {}],
+        // applying filter to exclude the document if match was applied
+        // and if the above step hasn't populated the fields
+        if (isMatchRequested) {
+          docsPipeline.push({
+            $match: {
+              $expr: {
+                $ne: ['$$ROOT', {}],
+              },
             },
-          },
-        });
+          });
+        }
       });
       if (postPopulateFilters) {
         docsPipeline.push({
@@ -166,14 +161,12 @@ const paginate = schema => {
     const countPromise = this.aggregate(countPipeline).exec();
 
     // to execute the pipeline
-    console.log("🚀 ~ file: paginate.js:171 ~ paginate ~ docsPipeline:", docsPipeline)
     const docsPromise = this.aggregate(docsPipeline).exec();
 
     // resolving the promises
     return Promise.all([countPromise, docsPromise]).then(values => {
       // seperating the counts and resulted docs
       const [counts, results] = values;
-      console.log("🚀 ~ file: paginate.js:167 ~ returnPromise.all ~ results:", results)
       const {totalResults = 0} = counts.length > 0 ? counts[0] : {};
       // defining total pages based on total results and limit
       const totalPages = Math.ceil(totalResults / limit);
