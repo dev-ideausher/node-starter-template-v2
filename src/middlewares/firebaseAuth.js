c237e1f244d52893c4d4e397ef3c57bc66f177d0
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:28:49 +0530

    upgraded: firebase auth middleware

--- src/middlewares/firebaseAuth.js
+++ src/middlewares/firebaseAuth.js
@@ -1,40 +1,56 @@
-const admin = require("firebase-admin");
-const httpStatus = require("http-status");
-const ApiError = require("../utils/ApiError");
+const admin = require('firebase-admin');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 
-const serviceAccount = require("../../firebase-service-secret.json");
-const { userService, authService } = require("../services");
-
+const serviceAccount = require('../../firebase-service-secret.json');
+const {authService} = require('../services');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const firebaseAuth = () => async (req, res, next) => {
-    return new Promise( async (resolve, reject) => {
-        try {
-            const token = req.headers?.authorization?.split(" ")[1]
-            // token not found
-            if(!token) { reject(new ApiError(httpStatus.BAD_REQUEST, "Please Authenticate!")); }
-            const payload = await admin.auth().verifyIdToken(token, true);
-            const user = await authService.getUserByFirebaseUId(payload.uid);
-            if(!user) {
-                if(req.path === "/register") {
-                    req.newUser = payload;
-                } else reject(new ApiError(httpStatus.NOT_FOUND, "User doesn't exist. Please create account"));
-            } else {
-                if(user.isBlocked) { throw new ApiError(httpStatus.FORBIDDEN, "User is blocked"); }
-                if(user.isDeleted) { throw new ApiError(httpStatus.GONE, "User doesn't exist anymore"); }
-                req.user = user;
-            }
-            
-            resolve(); 
-        } catch(err) {
-            console.log("FirebaseAuthError:", err);
-            reject(new ApiError(httpStatus.UNAUTHORIZED, "Failed to authenticate"))
+const firebaseAuth = (allowUserType = 'All') => async (req, res, next) => {
+  return new Promise(async (resolve, reject) => {
+    const token = req.headers?.authorization?.split(' ')[1];
+    console.log(token);
+    // token not found
+    if (!token) {
+      reject(new ApiError(httpStatus.BAD_REQUEST, 'Please Authenticate!'));
+    }
+    try {
+      const payload = await admin.auth().verifyIdToken(token, true);
+      console.log(payload);
+      const user = await authService.getUserByFirebaseUId(payload.uid);
+      if (!user) {
+        console.log(req.path);
+        if (['/register'].includes(req.path) || req.path.includes('secretSignup')) {
+          req.newUser = payload;
+          req.routeType = allowUserType;
+        } else reject(new ApiError(httpStatus.NOT_FOUND, "User doesn't exist. Please create account"));
+      } else {
+        if (!allowUserType.split(',').includes(user.__t) && allowUserType !== 'All') {
+          reject(new ApiError(httpStatus.FORBIDDEN, "Sorry, but you can't access this"));
         }
-    }).then(() => next()).catch((err) => next(err));
-}
+        if (user.isBlocked) {
+          reject(new ApiError(httpStatus.FORBIDDEN, 'User is blocked'));
+        }
+        if (user.isDeleted) {
+          reject(new ApiError(httpStatus.GONE, "User doesn't exist anymore"));
+        }
+        req.user = user;
+      }
 
+      resolve();
+    } catch (err) {
+      if (err.code === 'auth/id-token-expired') {
+        reject(new ApiError(httpStatus.UNAUTHORIZED, 'Session is expired'));
+      }
+      console.log('FirebaseAuthError:', err);
+      reject(new ApiError(httpStatus.UNAUTHORIZED, 'Failed to authenticate'));
+    }
+  })
+    .then(() => next())
+    .catch(err => next(err));
+};
 
-module.exports = firebaseAuth
+module.exports = firebaseAuth;
