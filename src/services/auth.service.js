977b363d5e50a254fdbf4931f60c24979bbfc540
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:30:38 +0530

    modified: auth service fn(s)

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,16 +1,16 @@
-const { User } = require("../models");
-const httpStatus = require("http-status");
-const ApiError = require("../utils/ApiError");
+const {User} = require('../models');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 
 async function createUser(user) {
-    return await User.create(user);
+  return await User.create(user);
 }
 
 async function getUserByFirebaseUId(id) {
-    return await User.findOne({ firebaseUid: id });
+  return await User.findOne({firebaseUid: id});
 }
 
 module.exports = {
-    createUser,
-    getUserByFirebaseUId
-}
+  createUser,
+  getUserByFirebaseUId,
+};
