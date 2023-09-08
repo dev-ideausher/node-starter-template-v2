244b91775fbe314b8d791aa79668cfb8e3ef7653
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:31:53 +0530

    formatted: server scripts

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
-const cors = require("cors");
-const express = require("express");
-const compression = require("compression");
+const cors = require('cors');
+const express = require('express');
+const compression = require('compression');
 
-const helmet = require("helmet");
+const helmet = require('helmet');
 
-const httpStatus = require("http-status");
-const routes = require("./routes/v1");
-const morgan = require("./config/morgan")
-const config = require("./config/config");
-const ApiError = require("./utils/ApiError");
-const { errorConverter, errorHandler } = require("./middlewares/error");
+const httpStatus = require('http-status');
+const routes = require('./routes/v1');
+const morgan = require('./config/morgan');
+const config = require('./config/config');
+const ApiError = require('./utils/ApiError');
+const {errorConverter, errorHandler} = require('./middlewares/error');
 
 const app = express();
 
-//Morgan will handle logging HTTP requests, 
+//Morgan will handle logging HTTP requests,
 // while winston logger will take care of your application-specific logs
-if(config.env!=="test"){
+if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
@@ -27,21 +27,21 @@ app.use(helmet());
 app.use(express.json());
 
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // gzip compression
 app.use(compression());
 
 // enable cors
 app.use(cors());
-app.options("*", cors());
+app.options('*', cors());
 
 // Reroute all API request starting with "/v1" route
-app.use("/v1",routes);
+app.use('/v1', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
 // convert error to ApiError, if needed
--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
-const app = require('./app')
-const config = require("./config/config")
+const app = require('./app');
+const config = require('./config/config');
 const logger = require('./config/logger');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 let server;
 
-mongoose.connect(config.mongoose.url,config.mongoose.options).then(()=>{
-    console.log("Connected to mongodb");
-}).catch((err)=>{
-    console.log(err)
-});
+mongoose
+  .connect(config.mongoose.url, config.mongoose.options)
+  .then(() => {
+    console.log('Connected to mongodb');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.listen(config.port, () => {
-    console.log(`BNeighbourly app listening on port ${config.port}!`)
+  console.log(`BNeighbourly app listening on port ${config.port}!`);
 });
-
 
 // ------------- Don't Modify  -------------
 const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        logger.info("Server closed");
-        process.exit(1);
-      });
-    } else {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
       process.exit(1);
-    }
-  };
-  
-  const unexpectedErrorHandler = (error) => {
-    logger.error(error);
-    exitHandler();
-  };
-  
-  process.on("uncaughtException", unexpectedErrorHandler);
-  process.on("unhandledRejection", unexpectedErrorHandler);
-  
-  process.on("SIGTERM", () => {
-    logger.info("SIGTERM received");
-    if (server) {
-      server.close();
-    }
-  });
-  // ------------- Don't Modify  -------------
-  
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = error => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
+// ------------- Don't Modify  -------------
