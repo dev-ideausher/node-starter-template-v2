a6f9142308650f9b4319a883f9b0250f111977fc
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:30:42 +0530

    modified: user service fn(s)

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,2 @@
-module.exports.userService = require("./user.service");
-module.exports.authService = require("./auth.service");
+module.exports.userService = require('./user.service');
+module.exports.authService = require('./auth.service');
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,45 +1,55 @@
-const { User } = require("../models");
-const httpStatus = require("http-status");
-const ApiError = require("../utils/ApiError");
+const {User} = require('../models');
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
 
-const userValidator = (user) => {
-    if(!user) { 
-        throw new ApiError(httpStatus.NOT_FOUND,"User not found.");
-    } else if(user.isDeleted) {
-        throw new ApiError(httpStatus.FORBIDDEN, "User account has been deleted.");
-    } else if(user.isBlocked) {
-        throw new ApiError(httpStatus.FORBIDDEN, "User has been blocked.");
-    }
+const userValidator = user => {
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found.');
+  } else if (user.isDeleted) {
+    throw new ApiError(httpStatus.FORBIDDEN, 'User account has been deleted.');
+  } else if (user.isBlocked) {
+    throw new ApiError(httpStatus.FORBIDDEN, 'User has been blocked.');
+  }
+};
+
+async function getUserById(id) {
+  const user = await User.findById(id);
+  userValidator(user);
+  return user;
+}
+
+async function getUsers(filters, options) {
+  return await User.paginate(filters, options);
 }
 
 async function updateUserById(id, newDetails) {
-    return await User.findByIdAndUpdate(id, newDetails, { returnDocument: "after" });
+  return await User.findByIdAndUpdate(id, newDetails, {new: true});
 }
 
 async function deleteUserById(id) {
-    try {
-        const user = await User.findById(id);
-        userValidator(user);
-        user.isDeleted = true;
-        await user.save();
-        return true;
-    } catch(err) {
-        throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, "Failed to delete the user");
-    }
+  try {
+    await User.findByIdAndDelete(id);
+    return true;
+  } catch (err) {
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Failed to delete the user');
+  }
 }
 
-async function updateUserPreferencesById(id, newPrefs) {
-    const user =  await User.findById(id);
-    userValidator(user);
-    Object.keys(newPrefs).map((key) => {
-        user.preferences[key] = newPrefs[key];
-    })
-    await user.save();
-    return user;
-} 
+async function updatePreferencesById(id, newPrefs) {
+  const user = await User.findById(id);
+  user.preferences = {
+    ...user.preferences,
+    ...newPrefs,
+  };
+  // any other fields if you have
+  await user.save();
+  return user;
+}
 
 module.exports = {
-    updateUserById,
-    deleteUserById,
-    updateUserPreferencesById
-}
+  getUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  updatePreferencesById,
+};
