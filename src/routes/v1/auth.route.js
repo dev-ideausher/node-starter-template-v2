8a0b2c4e211f58fb6b9ea3ec620c5b53545a3869
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:30:03 +0530

    modified: auth api(s)

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,13 +1,27 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const validate = require("../../middlewares/validate");
-const firebaseAuth = require("../../middlewares/firebaseAuth");
-const authValidation = require("../../validations/auth.validation");
+const validate = require('../../middlewares/validate');
+const firebaseAuth = require('../../middlewares/firebaseAuth');
+const authValidation = require('../../validations/auth.validation');
 
-const { authController } = require("../../controllers");
+const {authController} = require('../../controllers');
 
-router.post("/login", firebaseAuth(), authController.loginUser);
-router.post("/register", validate(authValidation.register), firebaseAuth(), authController.registerUser);
+router.post('/login', firebaseAuth('All'), authController.loginUser);
+
+router.post(
+  '/register',
+  firebaseAuth('Client'),
+  fileUploadService.multerUpload.single('profilePic'),
+  validate(authValidation.register),
+  authController.registerUser
+);
+
+router.post(
+  '/admin-secretSignup',
+  validate(authValidation.register),
+  firebaseAuth('Admin'),
+  authController.registerUser
+);
 
 module.exports = router;
