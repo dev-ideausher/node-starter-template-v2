814f947645ef925b0d1f8930f96759f46f42e3e1
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:27:34 +0530

    patch: reqd constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,13 @@
-const userRoles = ["user","owner","admin"];
-
 const dbOptions = {
-    page: 1,
-    limit: 10,
-    sortBy: "createdAt",
-    sortOrder: "asc"
+  page: 1,
+  limit: 10,
+  sortBy: 'createdAt',
+  sortOrder: 'asc',
 };
 
+const fileTypes = ['image/jpeg', 'image/jpg', 'image/png', 'video/mp4', 'application/pdf', 'application/msword'];
+
 module.exports = {
-    userRoles,
-    dbOptions
-}
+  dbOptions,
+  fileTypes,
+};
