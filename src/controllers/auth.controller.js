62a825223f51c67ae20908b921a995dbb85a033c
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:30:22 +0530

    modified: auth controller(s)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,35 +1,46 @@
-const { authService } = require("../services");
-const catchAsync = require("../utils/catchAsync");
+const {authService, favouriteService, userService} = require('../services');
+const catchAsync = require('../utils/catchAsync');
 
-const loginUser = catchAsync(async (req, res) => {
-    res.status(200).send({ data: req.user });
+const createNewUserObject = newUser => ({
+  email: newUser.email,
+  firebaseUid: newUser.uid,
+  profilePic: newUser.picture,
+  isEmailVerified: newUser.isEmailVerified,
+  firebaseSignInProvider: newUser.firebase.sign_in_provider,
 });
 
-const registerUser = catchAsync(async (req,res) => {
-    if(req.user) {
-        res.status(401).send({ message: "User already exist"});
-    } else {
-        if(req.newUser.firebase.sign_in_provider === "phone") {
-            userObj = { phone: req.newUser.phone_number, ...req.body }
-        } else {
-            userObj = {
-                name: req.newUser.name,
-                email: req.newUser.email,
-                profilePic: req.newUser.picture,
-                ...req.body
-            }    
-        }
-        userObj = { 
-            ...userObj,
-            firebaseUid: req.newUser.uid,
-            firebaseSignInProvider: req.newUser.firebase.sign_in_provider
-        }
-        const user = await authService.createUser(userObj);
-        res.status(201).send({ data: user });
+const loginUser = catchAsync(async (req, res) => {
+  const user = req.user.__t === 'Student' ? await userService.getStudent(req.user._id) : req.user;
+  res.status(200).send({data: req.user});
+});
+
+const registerUser = catchAsync(async (req, res) => {
+  if (req.user) {
+    res.status(401).send({message: 'User already exist'});
+    // } else if (!req.newUser.email_verified) {
+    //   res.status(401).send({ message: "Email not verified" });
+  } else {
+    const userObj = {
+      ...createNewUserObject(req.newUser),
+      ...req.body,
+    };
+    let user = null;
+    switch (req.routeType) {
+      case 'Client':
+        const favourites = await favouriteService.createFavourite();
+        user = await authService.createStudent({...userObj, favourites}, req.file);
+        break;
+      case 'Admin':
+        user = await authService.createAdmin(userObj);
+        break;
+      default:
+        break;
     }
+    res.status(201).send({data: user});
+  }
 });
 
 module.exports = {
-    loginUser,
-    registerUser
-}
+  loginUser,
+  registerUser,
+};
