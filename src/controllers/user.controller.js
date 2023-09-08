066b6bde33b296c7276e96b3bf665cceb1fe2879
Tushar Inani <tusharinani93@gmail.com>
2023-09-08 16:30:28 +0530

    modified: user controller(s)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,28 +1,37 @@
-const httpStatus = require("http-status");
-const ApiError = require("../utils/ApiError");
-const catchAsync = require("../utils/catchAsync");
-const { userService } = require("../services");
+const httpStatus = require('http-status');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const {userService} = require('../services');
 
 const updateUser = catchAsync(async (req, res) => {
-    const updatedUser = await userService.updateUserById(req.user._id, req.body);
-    res.status(200).send({ data: updatedUser, message: "Your details are updated" });
+  const updatedUser = await userService.updateUserById(req.user._id, req.body);
+  res.status(200).send({data: updatedUser, message: 'Your details are updated'});
 });
 
-const updateUserPreferences = catchAsync(async (req, res) => {
-    const updatedUser = await userService.updateUserPreferencesById(req.user._id, req.body);
-    res.status(200).send({ data: updatedUser, message: "Your preferences are saved" });
+const updatePreferences = catchAsync(async (req, res) => {
+  const updatedUser = await userService.updatePreferencesById(req.user._id, req.body);
+  res.status(200).send({data: updatedUser, message: 'Your preferences are updated'});
+});
+
+const softDeleteUser = catchAsync(async (req, res) => {
+  const {userId} = req.params;
+  if (req.user.__t !== 'Admin' && userId !== req.user._id.toString()) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Sorry, you are not authorized to do this');
+  }
+  await userService.markUserAsDeletedById(req.params.userId);
+  res.status(200).send({
+    message: 'User has been removed successfully.',
+  });
 });
 
 const deleteUser = catchAsync(async (req, res) => {
-    if(req.user.role === "user" && req.params.userId !== req.user._id.toString()) {
-        throw new ApiError(httpStatus.UNAUTHORIZED, "Sorry, you are not authorized to do this");
-    }
-    await userService.deleteUserById(req.params.userId);
-    res.status(201).send({ message: "The user deletion process has been completed successfully." });
+  await userService.deleteUserById(req.params.userId);
+  res.status(200).send({message: 'The user deletion process has been completed successfully.'});
 });
 
 module.exports = {
-    deleteUser,
-    updateUser,
-    updateUserPreferences,
-}
+  deleteUser,
+  updateUser,
+  softDeleteUser,
+  updatePreferences,
+};
