48b6807eb58682d324f9d6bcdab5148316c946f5
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2024-01-22 00:14:59 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,12 +2,44 @@ const dbOptions = {
   page: 1,
   limit: 10,
   sortBy: 'createdAt',
-  sortOrder: 'asc',
+  sortOrder: 'desc',
 };
 
-const fileTypes = ['image/jpeg', 'image/jpg', 'image/png', 'video/mp4', 'application/pdf', 'application/msword'];
+const imgTypeToExtension = {
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+  'image/svg': 'svg',
+  'image/svg+xml': 'svg+xml',
+};
+
+const docTypeToExtension = {
+  'text/plain': 'txt',
+  'application/pdf': 'pdf',
+  'application/msword': 'doc',
+  'application/vnd.ms-excel': 'xls',
+  'application/vnd.ms-powerpoint': 'ppt',
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+  'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
+};
+
+const mimetypeToExtension = {
+  ...imgTypeToExtension,
+  ...docTypeToExtension,
+};
+
+const imageTypes = Object.keys(imgTypeToExtension);
+const docTypes = Object.keys(docTypeToExtension);
+const fileTypes = [...imageTypes, ...docTypes];
 
 module.exports = {
   dbOptions,
+  imageTypes,
+  docTypes,
   fileTypes,
-};
+  imgTypeToExtension,
+  docTypeToExtension,
+  mimetypeToExtension 
+}
