e6ed91469433ade26520462ff4ec60d9edcced37
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2024-02-07 18:27:37 +0530

    Update export file of microservices

--- src/microservices/index.js
+++ src/microservices/index.js
@@ -1,3 +1,4 @@
 module.exports.smsService = require('./sms.service');
 module.exports.fileUploadService = require('./fileUpload.service');
 module.exports.notificationService = require('./notification.service');
+module.exports.paystack = require('./paystack.service');
