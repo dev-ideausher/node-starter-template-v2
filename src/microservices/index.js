3c12b935c008fd617235f303174655ec841ad39c
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2024-02-07 18:27:54 +0530

    Update index.js

--- src/microservices/index.js
+++ src/microservices/index.js
@@ -1,4 +1,4 @@
 module.exports.smsService = require('./sms.service');
 module.exports.fileUploadService = require('./fileUpload.service');
 module.exports.notificationService = require('./notification.service');
-module.exports.paystack = require('./paystack.service');
+module.exports.paystackService = require('./paystack.service');
