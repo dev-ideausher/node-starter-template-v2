feb40ba2dfd201559634035015bb03cd3a7b8502
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2024-05-15 20:58:26 +0530

    Update fileUpload.service.js

--- src/microservices/fileUpload.service.js
+++ src/microservices/fileUpload.service.js
@@ -2,7 +2,13 @@ const uuid = require('uuid').v4;
 const multer = require('multer');
 const storage = multer.memoryStorage();
 const {getSignedUrl} = require('@aws-sdk/s3-request-presigner');
-const {S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand} = require('@aws-sdk/client-s3');
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  DeleteObjectCommand,
+  ListObjectsV2Command,
+} = require('@aws-sdk/client-s3');
 
 const config = require('../config/config');
 const {fileTypes} = require('../constants');
@@ -30,9 +36,20 @@ const multerUpload = multer({
   // limits: { fileSize: 10000000, files: 10 },
 });
 
-async function getObjectURL(Key, signedUrl = false) {
-  const command = new GetObjectCommand({Key, Bucket: name});
-  const url = await getSignedUrl(s3client, command);
+/**
+ * Generates a URL for accessing an S3 object.
+ * @param {string} Key - The key (or path) of the S3 object.
+ * @param {boolean} [signedUrl=false] - Indicates whether to return a signed URL.
+ * @param {number} [expiresIn] - The duration (in seconds) for which the signed URL remains valid.
+ * @returns {Promise<{ key: string, url: string }>} An object containing the S3 object key and URL.
+ */
+
+async function getObjectURL(Key, signedUrl = false, expiresIn = 3600) {
+  const command = new GetObjectCommand({
+    Key,
+    Bucket: name,
+  });
+  const url = await getSignedUrl(s3client, command, {expiresIn});
   return {
     key: Key,
     url: signedUrl ? url : url.split('?')[0],
@@ -41,10 +58,10 @@ async function getObjectURL(Key, signedUrl = false) {
 
 async function s3Delete(Key) {
   const command = new DeleteObjectCommand({Key, Bucket: name});
-  return await s3client.send(command);
+  return s3client.send(command);
 }
 
-async function s3Upload(files, folder = 'uploads', private = false) {
+async function s3Upload(files, folder = 'uploads', private = false, expiresIn = 3600) {
   const params = files.map(file => {
     return {
       Bucket: name,
@@ -55,11 +72,14 @@ async function s3Upload(files, folder = 'uploads', private = false) {
   });
 
   return Promise.all(
-    params.map(param =>
+    params.map(async param =>
       s3client
         .send(new PutObjectCommand(param))
-        .then(async () => await getObjectURL(param.Key))
-        .catch(() => null)
+        .then(async () => getObjectURL(param.Key, private, expiresIn))
+        .catch(err => {
+          console.log(err);
+          throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, 'Failed to upload the media');
+        })
     )
   );
 }
